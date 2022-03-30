be02776275f8b0927d300bd539e89114aa28f4ae
masumbillah3416 <masumbillahsujon1998@gmail.com>
2022-03-30 23:55:26 +0600

    payment page

--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -1,37 +1,19 @@
-<link rel="stylesheet" href="{{ asset('/css/style.css') }}" />
-<script src="https://js.stripe.com/v3/"></script>
- 
-@if ($message = Session::get('success'))
-    <div class="success">
-        <strong>{{ $message }}</strong>
-    </div>
-@endif
- 
- 
-@if ($message = Session::get('error'))
-    <div class="error">
-        <strong>{{ $message }}</strong>
-    </div>
-@endif
- 
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
 <form action="{{ url('charge') }}" method="post" id="payment-form">
-    <div class="form-row">
-        <p><input type="text" name="amount" placeholder="Enter Amount" /></p>
-        <p><input type="email" name="email" placeholder="Enter Email" /></p>
-        <label for="card-element">
-        Credit or debit card
-        </label>
-        <div id="card-element">
-        <!-- A Stripe Element will be inserted here. -->
-        </div>
-       
-        <!-- Used to display form errors. -->
-        <div id="card-errors" role="alert"></div>
-    </div>
-    <p><button>Submit Payment</button></p>
     {{ csrf_field() }}
+    
+    <button type="submit">Submit Payment</button>
+    
 </form>
-<script>
-var publishable_key = '{{ env('STRIPE_KEY') }}';
-</script>
-<script src="{{ asset('/js/card.js') }}"></script>
+</body>
+</html>
